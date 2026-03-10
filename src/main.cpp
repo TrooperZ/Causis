5519c1fa5423c0281cccd2a2fa7d4abd71b14735
Amin Karic <amin.karic@gmail.com>
2026-03-09 22:10:30 -0500

    Initialized project

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "causis/AST.h"
+#include "causis/Interpreter.h"
+#include "causis/Token.h"
+
+#include <iostream>
+
+int main() {
+  std::cout << "Causis starter project configured.\n";
+  return 0;
+}
+
